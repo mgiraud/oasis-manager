e76d0198dd95c164cd0030a1ef3d35f843cacaf5
Marc Giraud-Telme <m.giraudtelme@pc-42.home>
2020-11-12 08:36:13 +0100

    ref #4 Back to universal app; finalize simple login/logout

--- app/store/index.js
+++ app/store/index.js
@@ -1,9 +1,16 @@
-import { call } from "../util/api";
+const cookieparser = process.server ? require('cookieparser') : undefined
 
 export const actions = {
-    async nuxtClientInit({ commit }) {
-        await call('/me').then((response) => {
-            commit('member/setAuth', response.ok)
-        })
-    }
+    nuxtServerInit({ commit }, { req }) {
+        let auth = false;
+        if (req.headers.cookie) {
+            const parsed = cookieparser.parse(req.headers.cookie)
+            try {
+                auth = !!parsed.BEARER;
+            } catch (err) {
+                // No valid cookie found
+            }
+        }
+        commit('member/setAuth', auth)
+    },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,12 +2,8 @@ require('dotenv').config()
 
 export default {
     srcDir: 'app/',
-    target: 'static',
     env: {
         apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:8000',
         apiBasePath: '/api'
     },
-    plugins: [
-        { src: '~/plugins/onInitClient', mode: 'client' }
-    ]
 }

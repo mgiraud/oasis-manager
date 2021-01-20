e8b264bd56b86dee4561417a6d60c6f3b7273835
Marc Giraud-Telme <m.giraudtelme@pc-25.home>
2021-01-20 08:21:21 +0100

    ref #36 Add mentions legales + link in footer menu

---
+++ app/pages/mentions_legales.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Mentions légales</v-card-title>
+          <v-card-text>
+            <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l’économie numérique, il est précisé aux utilisateurs du site XXX l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
+            <h3>Hébergeur</h3>
+
+            <p>Le site transalpins.fr est hébergé par la société XXX</p>
+            <p>Adresse:</p>
+            <p>Le stockage des données personnelles des Utilisateurs est exclusivement réalisé sur les centre de données (« clusters ») localisés en France de la société XXX </p>
+
+            <h3>Cookie</h3>
+            <p>Le seul cookie utilisé par ce site est généré lors de votre identification au site</p>
+            <v-card-text />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>

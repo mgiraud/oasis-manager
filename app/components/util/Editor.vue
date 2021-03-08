e7b0f6dc5d47fabc1804e8d707345c2c2be93253
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2021-03-08 14:27:18 +0100

    ref #42 Make toolbar, add some extensions

--- app/components/util/Editor.vue
+++ app/components/util/Editor.vue
@@ -1,50 +1,145 @@
 <template>
   <div>
     <div class="editor">
-      <v-toolbar v-if="editor" flat dark short>
-        <button :class="{ 'is-active': editor.isActive('bold') }" @click="editor.chain().focus().toggleBold().run()">
-          bold
-        </button>
-        <button :class="{ 'is-active': editor.isActive('italic') }" @click="editor.chain().focus().toggleBold().run()">
-          italic
-        </button>
-        <button :class="{ 'is-active': editor.isActive('italic') }" @click="editor.chain().focus().toggleBold().run()">
-          italic
-        </button>
-        <v-btn
-          small
-          :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }"
-          @click="editor.chain().focus().setTextAlign('left').run()"
-        >
-          <v-icon>mdi-format-align-left</v-icon>
-        </v-btn>
-        <v-btn
-          small
-          :class="{ 'is-active': editor.isActive({ textAlign: 'center' }) }"
-          @click="editor.chain().focus().setTextAlign('center').run()"
-        >
-          <v-icon>mdi-format-align-center</v-icon>
-        </v-btn>
-        <v-btn
-          small
-          :class="{ 'is-active': editor.isActive({ textAlign: 'right' }) }"
-          @click="editor.chain().focus().setTextAlign('right').run()"
-        >
-          <v-icon>mdi-format-align-right</v-icon>
-        </v-btn>
-        <v-btn
-          small
-          :class="{ 'is-active': editor.isActive({ textAlign: 'justify' }) }"
-          @click="editor.chain().focus().setTextAlign('justify').run()"
-        >
-          <v-icon>mdi-format-align-justify</v-icon>
-        </v-btn>
-        <v-btn
-          small
-          @click="editor.chain().focus().unsetTextAlign().run()"
-        >
-          Default
-        </v-btn>
+      <v-toolbar v-if="editor" flat dark>
+        <editor-btn
+          label="Gras"
+          :btn-class="{ 'grey darken-3': editor.isActive('bold') }"
+          :click-handler="() => editor.chain().focus().toggleBold().run()"
+          icon="mdi-format-bold"
+        />
+        <editor-btn
+          label="Italique"
+          :btn-class="{ 'grey darken-3': editor.isActive('italic') }"
+          :click-handler="() => editor.chain().focus().toggleItalic().run()"
+          icon="mdi-format-italic"
+        />
+        <editor-btn
+          label="Barrer"
+          :btn-class="{ 'grey darken-3': editor.isActive('strike') }"
+          :click-handler="() => editor.chain().focus().toggleStrike().run()"
+          icon="mdi-format-strikethrough"
+        />
+        <editor-btn
+          label="Supprimer le style de la sélection"
+          :click-handler="() => editor.chain().focus().unsetAllMarks().run()"
+          icon="mdi-format-clear"
+        />
+        <editor-btn
+          label="Supprimer le style du bloc"
+          :click-handler="() => editor.chain().focus().clearNodes().run()"
+          icon="mdi-layers-off"
+        />
+        <editor-btn
+          label="Barrer"
+          :btn-class="{ 'grey darken-3': editor.isActive('paragraph') }"
+          :click-handler="() => editor.chain().focus().setParagraph().run()"
+          icon="mdi-format-pilcrow"
+        />
+        <editor-btn
+          label="Taille xxl"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 1 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 1 }).run()"
+          icon="mdi-format-header-1"
+        />
+        <editor-btn
+          label="Taille xl"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 2 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 2 }).run()"
+          icon="mdi-format-header-2"
+        />
+        <editor-btn
+          label="Taille l"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 3 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 3 }).run()"
+          icon="mdi-format-header-3"
+        />
+        <editor-btn
+          label="Taille m"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 4 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 4 }).run()"
+          icon="mdi-format-header-4"
+        />
+        <editor-btn
+          label="Taille s"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 5 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 5 }).run()"
+          icon="mdi-format-header-5"
+        />
+        <editor-btn
+          label="Taille xs"
+          :btn-class="{ 'grey darken-3': editor.isActive('heading', { level: 6 }) }"
+          :click-handler="() => editor.chain().focus().toggleHeading({ level: 6 }).run()"
+          icon="mdi-format-header-6"
+        />
+        <editor-btn
+          label="Liste"
+          :btn-class="{ 'grey darken-3': editor.isActive('bulletList') }"
+          :click-handler="() => editor.chain().focus().toggleBulletList().run()"
+          icon="mdi-format-list-bulleted"
+        />
+        <editor-btn
+          label="Liste numérotée"
+          :btn-class="{ 'grey darken-3': editor.isActive('orderedList') }"
+          :click-handler="() => editor.chain().focus().toggleOrderedList().run()"
+          icon="mdi-format-list-numbered"
+        />
+        <editor-btn
+          label="Bloc de code"
+          :btn-class="{ 'grey darken-3': editor.isActive('codeBlock') }"
+          :click-handler="() => editor.chain().focus().toggleCodeBlock().run()"
+          icon="mdi-code-tags"
+        />
+        <editor-btn
+          label="Citation"
+          :btn-class="{ 'grey darken-3': editor.isActive('blockquote') }"
+          :click-handler="() => editor.chain().focus().toggleCodeBlock().run()"
+          icon="mdi-format-quote-close"
+        />
+        <editor-btn
+          label="Séparateur horizontal"
+          :click-handler="() => editor.chain().focus().setHorizontalRule().run()"
+          icon="mdi-minus"
+        />
+        <editor-btn
+          label="Retour à la ligne"
+          :click-handler="() => editor.chain().focus().setHardBreak().run()"
+          icon="mdi-keyboard-return"
+        />
+        <editor-btn
+          label="Undo"
+          :click-handler="() => editor.chain().focus().undo().run()"
+          icon="mdi-undo"
+        />
+        <editor-btn
+          label="Redo"
+          :click-handler="() => editor.chain().focus().redo().run()"
+          icon="mdi-redo"
+        />
+        <editor-btn
+          label="Aligner à gauche"
+          :btn-class="{ 'grey darken-3': editor.isActive({ textAlign: 'left' }) }"
+          :click-handler="() => editor.chain().focus().setTextAlign('left').run()"
+          icon="mdi-format-align-left"
+        />
+        <editor-btn
+          label="Centrer"
+          :btn-class="{ 'grey darken-3': editor.isActive({ textAlign: 'center' }) }"
+          :click-handler="() => editor.chain().focus().setTextAlign('center').run()"
+          icon="mdi-format-align-center"
+        />
+        <editor-btn
+          label="Aligner à droite"
+          :btn-class="{ 'grey darken-3': editor.isActive({ textAlign: 'right' }) }"
+          :click-handler="() => editor.chain().focus().setTextAlign('right').run()"
+          icon="mdi-format-align-right"
+        />
+        <editor-btn
+          label="Justifier"
+          :btn-class="{ 'grey darken-3': editor.isActive({ textAlign: 'justify' }) }"
+          :click-handler="() => editor.chain().focus().setTextAlign('justify').run()"
+          icon="mdi-format-align-justify"
+        />
       </v-toolbar>
       <editor-content class="editor__content" :editor="editor" />
     </div>
@@ -71,6 +166,14 @@
     }
   }
 }
+
+.v-toolbar {
+  ::v-deep {
+    .v-toolbar__content {
+      flex-wrap: wrap;
+    }
+  }
+}
 </style>
 
 <script>
@@ -78,10 +181,14 @@
 import { Editor, EditorContent } from '@tiptap/vue-2'
 import { defaultExtensions } from '@tiptap/starter-kit'
 import TextAlign from '@tiptap/extension-text-align'
+import EditorBtn from '@/components/util/Editor/EditorBtn'
+import Gapcursor from '@tiptap/extension-gapcursor'
+import Typography from '@tiptap/extension-typography'
 
 export default {
   components: {
-    EditorContent
+    EditorContent,
+    EditorBtn
   },
   props: {
     value: {
@@ -107,7 +214,7 @@ export default {
   mounted () {
     this.editor = new Editor({
       content: this.value,
-      extensions: [...defaultExtensions(), TextAlign]
+      extensions: [...defaultExtensions(), TextAlign, Gapcursor, Typography]
     })
 
     this.editor.on('update', () => {
---
+++ app/components/util/Editor/EditorBtn.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-tooltip top>
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn
+        small
+        :class="btnClass"
+        v-bind="attrs"
+        @click="clickHandler"
+        v-on="on"
+      >
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </template>
+    <span>{{ label }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    btnClass: {
+      type: Object,
+      required: false,
+      default: null
+    },
+    clickHandler: {
+      type: Function,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    editor: null
+  })
+}
+</script>

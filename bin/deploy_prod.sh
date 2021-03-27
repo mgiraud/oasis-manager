311d8ffb94536f982c880a036b0eaced617748ca
Marc Giraud-Telme <m.giraudtelme@imac-de-marc.home>
2021-03-27 10:25:44 +0100

    ref #37 Update deploy action

--- bin/deploy_prod.sh
+++ bin/deploy_prod.sh
@@ -2,6 +2,8 @@
 rm config/jwt/*.pem
 ln -s ${APP_CONGIG_PATH}/jwt/private.pem ./config/jwt/private.pem
 ln -s ${APP_CONGIG_PATH}/jwt/public.pem ./config/jwt/public.pem
+
+rm .env.local
 ln -s ${APP_CONGIG_PATH}/.env.local .env.local
 
 export APP_ENV=prod

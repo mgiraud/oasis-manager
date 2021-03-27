ab009f9c6cbff8949f1742c00719109832815eaf
Marc Giraud-Telme <m.giraudtelme@imac-de-marc.home>
2021-03-27 06:23:01 +0100

    ref #37 add apache-pack

---
+++ bin/deploy_prod.sh
@@ -0,0 +1,8 @@
+# Deal with JWT SSH keys
+rm config/jwt/*.pem
+ln -s ${APP_CONGIG_PATH}/jwt/private.pem ./config/jwt/private.pem
+ln -s ${APP_CONGIG_PATH}/jwt/public.pem ./config/jwt/public.pem
+ln -s ${APP_CONGIG_PATH}/.env.local .env.local
+
+export APP_ENV=prod
+export APP_DEBUG=0

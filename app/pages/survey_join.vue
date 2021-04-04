bac4cfca3271ab27a9d248c6447c5c01a60f8dda
mgiraud <11818237+mgiraud@users.noreply.github.com>
2021-04-04 09:09:05 +0200

    ref #56 Add dynamic page for survey-join

--- app/pages/survey_join.vue
+++ app/pages/survey_join.vue
@@ -4,7 +4,28 @@
       <v-col cols="12">
         <v-card>
           <v-card-title>Tu souhaites nous rejoindre ?</v-card-title>
-          <v-card-text>
+          <v-card-text >
+            <div v-if="page" v-html=page.content />     
+            <v-container v-else>
+              <v-row>
+                <v-col cols="12">
+                  Avant de remplir le question, merci de prendre connaissance des éléments suivants :
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="6">
+                  <v-img :src="require(`~/assets/img/join_process_1.png`)" contain />
+                </v-col>
+                <v-col cols="6">
+                  <v-img :src="require(`~/assets/img/join_process_2.png`)" contain />
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12">
+                  Tu peux remplir ce questionnaire en ligne ou bien <a :href="downloadPdfUrl">Télécharger le PDF</a> pour le remplir numériquement ou manuellement et nous l'envoyer à l'addresse suivante : @AJOUTER
+                </v-col>
+              </v-row>
+            </v-container>
             <survey-join-form ref="createForm" :values="item" :errors="violations" />
             <toolbar :handle-submit="onSendForm" :handle-reset="resetForm" />
             <Loading :visible="isLoading" />
@@ -23,9 +44,12 @@ import Loading from '../components/util/Loading.vue'
 import NotificationMixin from '~/mixins/notification'
 import { SurveyJoin } from '~/store/survey_join'
 import { ElementWithValidation } from '~/vue-shim'
+import Template from '~/components/page/Template.vue'
+import { Page } from '~/store/page'
 
 const surveyJoinModule = namespace('survey_join')
 const notificationModule = namespace('notifications')
+const pageModule = namespace('page')
 
 @Component({
   components: {
@@ -62,6 +86,12 @@ export default class SurveyJoinPage extends mixins(NotificationMixin) {
   @surveyJoinModule.Action('reset') reset !: () => void
   @surveyJoinModule.Action('create') createSurvey!: (survey: SurveyJoin) => SurveyJoin;
   @notificationModule.Action('setTimeout') setTimeoutDuration!: (show: number) => void
+
+  @pageModule.Getter('find') find!: (url: string) => Page | null
+
+  get page (): Page | null {
+    return this.find('/api/pages/survey-join')
+  };
 
   create (data: SurveyJoin) {
     data.motivations = data.motivationsRaw.map(motivation => motivation.id)

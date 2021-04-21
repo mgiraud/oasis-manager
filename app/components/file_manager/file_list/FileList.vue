9752d02ab8222365365dc8fd03c4ee9aa7aa0bde
Marc Giraud-Telme <m.giraudtelme@mbp-de-marc.home>
2021-04-21 09:05:57 +0200

    ref #72 Remove from selection

--- app/components/file_manager/file_list/FileList.vue
+++ app/components/file_manager/file_list/FileList.vue
@@ -14,13 +14,40 @@
       <v-col>
         <v-container>
           <v-row no-gutters>
-            <v-col v-for="(thumbnail, i) in thumbnails" :key="i">
-              <v-img :src="thumbnail.src" max-height="200" max-width="200" />
+            <v-col v-for="(thumbnail, i) in thumbnails" :key="i" cols="4" align-self="center">
+              <v-card>
+                <v-img :src="thumbnail.src" max-height="200" contain />
+                <v-card-actions>
+                  <v-btn
+                    class="btn-remove-thumbnail"
+                    fab
+                    dark
+                    x-small
+                    color="error"
+                    @click="removeThumbnail(i)"
+                  >
+                    <v-icon dark>
+                      ri-close-line
+                    </v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
             </v-col>
           </v-row>
           <v-row>
             <v-col v-for="(link, i) in links" :key="i">
-              <span>{{ link.name }}</span>
+              <span>{{ link.name }}<v-btn
+                class="btn-remove-thumbnail"
+                fab
+                dark
+                x-small
+                color="error"
+                @click="removeLink(i)"
+              >
+                <v-icon dark>
+                  ri-close-line
+                </v-icon>
+              </v-btn></span>
             </v-col>
           </v-row>
         </v-container>
@@ -37,6 +64,8 @@ import { Link, Thumbnail } from '../FileManager.vue'
 export default class FileList extends Vue {
     @Prop({ type: Array, required: true }) readonly thumbnails!: Thumbnail[]
     @Prop({ type: Array, required: true }) readonly links!: Link[]
+    @Prop({ type: Function, required: true }) readonly removeThumbnail!: (index: number) => {}
+    @Prop({ type: Function, required: true }) readonly removeLink!: (index: number) => {}
 
     openFileSelection () {
       (this.$refs.fileSelection as HTMLInputElement).click()

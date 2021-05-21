d5f0de6ee412109476d3c964c6b7e1e6732cd3f6
mgiraud <11818237+mgiraud@users.noreply.github.com>
2021-05-21 08:06:44 +0200

    ref #55 Update pageLog entity and update page subscriber

--- src/Entity/PageLog.php
+++ src/Entity/PageLog.php
@@ -5,35 +5,66 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PageLogRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     attributes={
+ *          "normalization_context"={"groups"={"page_log:read"}},
+ *          "denormalization_context"={"groups"={"page_log:write"}},
+ *     },
+ *     collectionOperations={
+ *         "get"={"method"="GET"},
+ *         "post"={"security"="is_granted('USER_CAN_EDIT_PAGE')"},
+ *     },
+ *     itemOperations={
+ *         "get"={"method"="GET"},
+ *     }
+ * )
  * @ORM\Entity(repositoryClass=PageLogRepository::class)
  */
 class PageLog
 {
+    const LOGS_PER_PAGE = 3;
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"page_log:read"})
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity=Member::class)
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"page_log:read"})
      */
     private $updatedBy;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups({"page_log:read"})
      */
     private $updatedAt;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"page_log:read", "page_log:write"})
      */
     private $originalContent;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=false, options={"default"=false})
+     * @Groups({"page_log:read", "page_log:write"})
+     */
+    private $draft;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Page::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $page;
 
     public function getId(): ?int
     {
@@ -72,6 +103,30 @@ class PageLog
     public function setOriginalContent(string $originalContent): self
     {
         $this->originalContent = $originalContent;
+
+        return $this;
+    }
+
+    public function isDraft(): bool
+    {
+        return $this->draft;
+    }
+
+    public function setIsDraft(bool $draft): self
+    {
+        $this->draft = $draft;
+
+        return $this;
+    }
+
+    public function getPage(): ?Page
+    {
+        return $this->page;
+    }
+
+    public function setPage(?Page $page): self
+    {
+        $this->page = $page;
 
         return $this;
     }
--- src/EventSubscriber/PageSubscriber.php
+++ src/EventSubscriber/PageSubscriber.php
@@ -7,6 +7,8 @@ namespace App\EventSubscriber;
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\Member;
 use App\Entity\Page;
+use App\Entity\PageLog;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -15,10 +17,12 @@ use Symfony\Component\Security\Core\Security;
 class PageSubscriber implements EventSubscriberInterface
 {
     private Security $security;
+    private EntityManagerInterface $manager;
 
-    public function __construct(Security $security)
+    public function __construct(Security $security, EntityManagerInterface $manager)
     {
         $this->security = $security;
+        $this->manager = $manager;
     }
 
     public static function getSubscribedEvents()
@@ -26,26 +30,85 @@ class PageSubscriber implements EventSubscriberInterface
         return [
             KernelEvents::VIEW => [
                 ['beforePageValidated', EventPriorities::PRE_VALIDATE],
+                ['afterPageWritten', EventPriorities::POST_WRITE],
             ]
         ];
     }
 
     public function beforePageValidated(ViewEvent $event)
     {
-        if ('POST' !== $event->getRequest()->getMethod()) {
+        if (!$this->canWrite($event)) {
             return;
         }
 
         $page = $event->getControllerResult();
-        if (!$page instanceof Page) {
+        $user = $this->security->getUser();
+        $page->setCreatedBy($user);
+    }
+
+    public function afterPageWritten(ViewEvent $event)
+    {
+        if (!$this->canEdit($event)) {
             return;
+        }
+        /** @var Page $page */
+        $page = $event->getControllerResult();
+        /** @var Member $user */
+        $user = $this->security->getUser();
+
+        $this->manager->getConnection()->executeQuery('
+            DELETE FROM page_log WHERE id IN (
+                SELECT id from page_log p2 
+                WHERE draft = FALSE
+                ORDER BY updated_at DESC 
+                OFFSET :nbLogPerPage
+            )
+        ', [
+            'nbLogPerPage' => PageLog::LOGS_PER_PAGE - 1
+        ]);
+
+        $pageLog = new PageLog();
+        $pageLog
+            ->setUpdatedBy($user)
+            ->setUpdatedAt(new \Datetime())
+            ->setIsDraft(false)
+            ->setOriginalContent($page->getContent())
+            ->setPage($page)
+        ;
+        $this->manager->persist($pageLog);
+        $this->manager->flush();
+    }
+
+    private function canEdit(ViewEvent $event): bool
+    {
+        if ('PUT' !== $event->getRequest()->getMethod()) {
+            return false;
+        }
+
+        return $this->canUpdate($event);
+    }
+
+    private function canWrite(ViewEvent $event): bool
+    {
+        if ('POST' !== $event->getRequest()->getMethod()) {
+            return false;
+        }
+
+        return $this->canUpdate($event);
+    }
+
+    public function canUpdate(ViewEvent $event)
+    {
+        $page = $event->getControllerResult();
+        if (!$page instanceof Page) {
+            return false;
         }
 
         $user = $this->security->getUser();
         if (!$user instanceof Member) {
-            return;
+            return false;
         }
 
-        $page->setCreatedBy($user);
+        return true;
     }
 }

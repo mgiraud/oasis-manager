1134416021def5e07ef7cc1d2c32b3c86ecc7211
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2021-06-29 17:00:36 +0200

    Build

--- .nuxt/dist/server/pages/admin/blog-article/new.js
+++ .nuxt/dist/server/pages/admin/blog-article/new.js
@@ -8501,12 +8501,12 @@ installComponents_default()(Editor_component, {VBtn: VBtn["a" /* default */],VTo
 
 "use strict";
 
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/components/admin/blog-article/Form.vue?vue&type=template&id=1d4c7150&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-form',[_c('v-container',[_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('v-text-field',{attrs:{"label":"Titre","error-messages":_vm.titleErrors,"required":""},on:{"input":function($event){return _vm.v$.title.$touch()},"blur":function($event){return _vm.v$.title.$touch()}},model:{value:(_vm.item.title),callback:function ($$v) {_vm.$set(_vm.item, "title", $$v)},expression:"item.title"}})],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',{attrs:{"cols":"12","md":"6"}},[_c('v-switch',{attrs:{"label":"Publier","input-value":"true"},model:{value:(_vm.item.isPublished),callback:function ($$v) {_vm.$set(_vm.item, "isPublished", $$v)},expression:"item.isPublished"}})],1),_vm._v(" "),_c('v-col',{attrs:{"cols":"12","sm":"6","md":"6"}},[(_vm.item.tags)?_c('v-combobox',{attrs:{"items":_vm.item.tags,"no-data-text":"Aucun tag","label":"Tags","return-object":false,"clearable":"","multiple":""},model:{value:(_vm.item.tags),callback:function ($$v) {_vm.$set(_vm.item, "tags", $$v)},expression:"item.tags"}}):_vm._e()],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',[(_vm.state.mediaNodes)?_c('v-combobox',{attrs:{"items":_vm.state.mediaNodes,"no-data-text":"Aucun galerie n'a ce nom","label":"Lier une galerie à cette page","item-text":"name","item-value":"@id","return-object":false,"clearable":""},model:{value:(_vm.item.mediaNode),callback:function ($$v) {_vm.$set(_vm.item, "mediaNode", $$v)},expression:"item.mediaNode"}}):_vm._e()],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('ClientOnly',[(_vm.item.content !== undefined)?_c('Editor',{model:{value:(_vm.item.content),callback:function ($$v) {_vm.$set(_vm.item, "content", $$v)},expression:"item.content"}}):_vm._e()],1)],1)],1)],1)],1)}
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/components/admin/blog-article/Form.vue?vue&type=template&id=c7d8d022&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-form',[_c('v-container',[_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('v-text-field',{attrs:{"label":"Titre","error-messages":_vm.titleErrors,"required":""},on:{"input":function($event){return _vm.v$.title.$touch()},"blur":function($event){return _vm.v$.title.$touch()}},model:{value:(_vm.item.title),callback:function ($$v) {_vm.$set(_vm.item, "title", $$v)},expression:"item.title"}})],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',{attrs:{"cols":"12","md":"6"}},[_c('v-switch',{attrs:{"label":"Publier","input-value":"true"},model:{value:(_vm.item.isPublished),callback:function ($$v) {_vm.$set(_vm.item, "isPublished", $$v)},expression:"item.isPublished"}})],1),_vm._v(" "),_c('v-col',{attrs:{"cols":"12","sm":"6","md":"6"}},[(_vm.item.tags)?_c('v-combobox',{attrs:{"items":_vm.item.tags,"no-data-text":"Aucun tag","label":"Tags","return-object":false,"clearable":"","multiple":""},model:{value:(_vm.item.tags),callback:function ($$v) {_vm.$set(_vm.item, "tags", $$v)},expression:"item.tags"}}):_vm._e()],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',[(_vm.state.selectItems)?_c('v-combobox',{attrs:{"items":_vm.state.selectItems,"no-data-text":"Aucun galerie n'a ce nom","label":"Lier une galerie à cette page","item-text":"name","item-value":"@id","return-object":false,"clearable":""},model:{value:(_vm.item.mediaNode),callback:function ($$v) {_vm.$set(_vm.item, "mediaNode", $$v)},expression:"item.mediaNode"}}):_vm._e()],1)],1),_vm._v(" "),_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('ClientOnly',[(_vm.item.content !== undefined)?_c('Editor',{model:{value:(_vm.item.content),callback:function ($$v) {_vm.$set(_vm.item, "content", $$v)},expression:"item.content"}}):_vm._e()],1)],1)],1)],1)],1)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./app/components/admin/blog-article/Form.vue?vue&type=template&id=1d4c7150&
+// CONCATENATED MODULE: ./app/components/admin/blog-article/Form.vue?vue&type=template&id=c7d8d022&
 
 // EXTERNAL MODULE: ./node_modules/@nuxtjs/composition-api/dist/runtime/index.mjs + 1 modules
 var runtime = __webpack_require__(3);
@@ -8588,7 +8588,7 @@ var Editor = __webpack_require__(327);
       return errors;
     });
     Object(runtime["h" /* onMounted */])(() => {
-      MediaNodeStore["a" /* mediaNodeStore */].fetchAll();
+      MediaNodeStore["a" /* mediaNodeStore */].fetchSelectItems();
     });
     return {
       item,
@@ -8672,12 +8672,12 @@ installComponents_default()(component, {VCol: VCol["a" /* default */],VCombobox:
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/pages/admin/blog-article/new.vue?vue&type=template&id=799d8684&
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/pages/admin/blog-article/new.vue?vue&type=template&id=1090e88f&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('Form',{ref:"createForm",attrs:{"values":_vm.item,"errors":_vm.state.violations}}),_vm._ssrNode(" "),_c('Toolbar',{attrs:{"handle-submit":_vm.onSendForm,"handle-reset":_vm.resetForm}}),_vm._ssrNode(" "),_c('Loading',{attrs:{"visible":_vm.state.isLoading}})],2)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./app/pages/admin/blog-article/new.vue?vue&type=template&id=799d8684&
+// CONCATENATED MODULE: ./app/pages/admin/blog-article/new.vue?vue&type=template&id=1090e88f&
 
 // EXTERNAL MODULE: ./node_modules/@nuxtjs/composition-api/dist/runtime/index.mjs + 1 modules
 var runtime = __webpack_require__(3);
@@ -8718,7 +8718,8 @@ var BlogArticleStore = __webpack_require__(322);
   setup() {
     const item = Object(runtime["k" /* reactive */])({
       content: '',
-      isPublished: false
+      isPublished: false,
+      tags: []
     });
     BlogArticleStore["a" /* blogArticleStore */].setContext(Object(runtime["q" /* useContext */])());
     return {

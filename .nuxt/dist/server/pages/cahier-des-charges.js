1210b627170c763c9c0d1fab9bfd9ea353ab5859
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2022-01-20 07:23:50 +0100

    Build

--- .nuxt/dist/server/pages/cahier-des-charges.js
+++ .nuxt/dist/server/pages/cahier-des-charges.js
@@ -8,12 +8,12 @@ exports.modules = {
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/pages/cahier-des-charges.vue?vue&type=template&id=729eb611&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-container',{attrs:{"fluid":""}},[_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('v-card',[_c('v-card-title',[_vm._v("Cahier des charges !")]),_vm._v(" "),_c('v-card-text',[_c('v-container',[_c('v-row',[_c('v-col',{staticClass:"text-center"},[_c('iframe',{staticStyle:{"border":"none"},attrs:{"width":"824","height":"1000","src":"https://docs.google.com/document/d/e/2PACX-1vR1uRoEHWqkGt86ELhUzcfGs2Bm9u0QnD3Yi8vmqFHiCrkuXkf02hQigct6fBelCQ/pub?embedded=true"}})])],1)],1)],1)],1)],1)],1)],1)}
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./app/pages/cahier-des-charges.vue?vue&type=template&id=82bcacf6&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('v-container',{attrs:{"fluid":""}},[_c('v-row',[_c('v-col',{attrs:{"cols":"12"}},[_c('v-card',[_c('v-card-title',[_vm._v("Cahier des charges !")]),_vm._v(" "),_c('v-card-text',[_c('v-container',[_c('v-row',[_c('v-col',{staticClass:"text-center"},[_c('iframe',{staticStyle:{"border":"none"},attrs:{"width":"824","height":"1000","src":"https://docs.google.com/document/d/e/2PACX-1vSQSq2qXeczXkvgoKHm80Rr6O8UuutoArPrqFvQwjfjcJfzIQJQPpEf34-iGpdOZA/pub?embedded=true"}})])],1)],1)],1)],1)],1)],1)],1)}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./app/pages/cahier-des-charges.vue?vue&type=template&id=729eb611&
+// CONCATENATED MODULE: ./app/pages/cahier-des-charges.vue?vue&type=template&id=82bcacf6&
 
 // EXTERNAL MODULE: ./node_modules/@nuxtjs/composition-api/dist/runtime/index.mjs + 1 modules
 var runtime = __webpack_require__(3);

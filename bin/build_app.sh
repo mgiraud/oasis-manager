b42bbf04c889d38c11be4c53b6d97222fcf749d9
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2022-05-16 17:28:41 +0200

    Add server.js + build

--- bin/build_app.sh
+++ bin/build_app.sh
@@ -1,4 +1,5 @@
 NODE_ENV=production API_BASE_URL=https://api.lestransalpins.org/api yarn build
+cp ./bin/server.js .output/server/
 
 ## A faire sur le serveur
 # cloudlinux-selector restart  --json --interpreter nodejs --app-root public_html/oasis-manager

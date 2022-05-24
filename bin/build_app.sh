2e2148a4870a684cc0dc74ee564f7ddb73910323
Marc Giraud-Telme <m.giraudtelme@iMac-de-Marc.local>
2022-05-24 14:45:45 +0200

    Remove server node_modules

--- bin/build_app.sh
+++ bin/build_app.sh
@@ -1,7 +1,6 @@
 NODE_ENV=production API_BASE_URL=https://api.lestransalpins.org/api yarn build
 cp ./bin/server.js .output/server/
 rm -rf ./.output/server/node_modules
-touch ./.output/server/node_modules
 
 ## A faire sur le serveur
 # cloudlinux-selector restart  --json --interpreter nodejs --app-root public_html/oasis-manager
